efd0b3dda98254903fe461b9dfc3803f70b030bd
Gustavo I. López <lopezgus@gmail.com>
2023-05-06 11:33:51 -0300

    commit exercise 2.6

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -7,20 +7,50 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const Personas = ({ persona }) => <p> {persona.name}  </p>;
+  ;
+
+  const handlenewName = (event) => {
+    setNewName (event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const PhoneObject = {
+      name: newName
+    }
+    setPersons (persons.concat(PhoneObject));
+    console.log(persons);
+    setNewName('');
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handlenewName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <>
+        <div>
+          {persons.map((person,i) => 
+            <Personas key={i} persona={person} />
+          )}
+        </div>
+      </>
+{/*
+      <div>
+        <p> debug NewName: {newName}</p>
+        
+      </div>
+*/}      
     </div>
+    
   )
 }
 
