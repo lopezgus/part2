ab0cc302048233da22f57e29cf2402d4b5212797
Gustavo I. López <lopezgus@gmail.com>
2023-05-08 22:02:09 -0300

    Commit Part 2 exercise 2.8

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,34 +2,53 @@ import React, { useState } from 'react'
 import './App.css';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+//    { name: 'Ada Lovelace', number: '39-44-5323523' },
+//    { name: 'Dan Abramov', number: '12-43-234345' },
+//    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
-  const Personas = ({ persona }) => <p> {persona.name}  </p>;
-  ;
+//debug
+//  console.log("debug", persons);
+  // React Componet
+  const Personas = ({ persona }) => {
+//    console.log(persona);
+    return <p> {persona.name}  {persona.number}</p>
+  };
 
+  //handler of inputs: names
   const handlenewName = (event) => {
-    setNewName (event.target.value)
+    setNewName(event.target.value);
+  }
+
+  //handler of inputs: numbers
+  const handlenewNumber = (event) => {
+    setNewNumber(event.target.value);
+
   }
 
   const addPerson = (event) => {
     event.preventDefault()
 
-    if (persons.find(pers => pers.name.toLowerCase()===newName.toLowerCase())) {
+    if (persons.find(pers => pers.name.toLowerCase() === newName.toLowerCase())) {
 
       alert(` ${newName} is already added to phonebook `);
       setNewName('');
+      setNewNumber('');
       return;
     }
 
-    const PhoneObject = {
-      name: newName
-    }
-    setPersons (persons.concat(PhoneObject));
-    console.log(persons);
+    const PhoneObject = [
+      { name: newName , 
+        number: newNumber }
+    ]
+    setPersons(persons.concat(PhoneObject));
+//    console.log(persons);
     setNewName('');
+    setNewNumber('');
   }
 
   return (
@@ -40,25 +59,28 @@ const App = () => {
           name: <input value={newName} onChange={handlenewName} />
         </div>
         <div>
+          Number: <input value={newNumber} onChange={handlenewNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <>
         <div>
-          {persons.map((person,i) => 
+          {persons.map((person, i) =>
             <Personas key={i} persona={person} />
           )}
         </div>
       </>
-{/*
+      {/*
       <div>
         <p> debug NewName: {newName}</p>
         
       </div>
-*/}      
+*/}
     </div>
-    
+
   )
 }
 
